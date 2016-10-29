628271f3f4ab69a68007c426c8ff6c71c52ce73a
Marek Marecki <s14288@pjwstk.edu.pl>
2016-10-29 11:29:22 +0200

    Add Address model

---
+++ src/main/java/domain/Address.java
@@ -0,0 +1,27 @@
+package domain;
+
+public class Address {
+
+	private String city;
+	private String street;
+	private int house_number;
+
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public int getHouseNumber() {
+		return house_number;
+	}
+	public void setHouseNumber(int house_number) {
+		this.house_number = house_number;
+	}
+}
