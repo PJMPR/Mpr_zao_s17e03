1113ec7b7796b44dc6ddc8257045727fd46e2b24
student <student@virtualbox>
2016-10-29 11:30:31 +0200

    filmbase

--- src/main/java/domain/App.java
+++ src/main/java/domain/App.java
@@ -1,11 +1,13 @@
 package domain;
 
 import dao.PersonRepository;
+import dao.filmlist;
 
 public class App 
 {
     public static void main( String[] args )
     {
     	PersonRepository repo = new PersonRepository();
+    	filmlist repo2 = new filmlist();
     }
 }
