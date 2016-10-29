f44863746fbaead7c2191ed5fa387b2d96c16b35
student <student@virtualbox>
2016-10-29 11:50:43 +0200

    add logic

--- src/main/java/domain/App.java
+++ src/main/java/domain/App.java
@@ -1,5 +1,6 @@
 package domain;
 
+import dao.AddressRepository;
 import dao.PersonRepository;
 
 public class App 
@@ -7,5 +8,14 @@ public class App
     public static void main( String[] args )
     {
     	PersonRepository repo = new PersonRepository();
+    	AddressRepository addressRepo = new AddressRepository();
+    	
+    	Person jan = new Person();
+    	jan.setName("Jan");
+    	jan.setSurname("Kowalski");
+    	jan.setAge(30);
+    	
+    	repo.add(jan);
+    	System.out.print("Koniec");
     }
 }
