75d38aad2bac6cf1de57704d2fc9963fcfce4296
Marek Marecki <s14288@pjwstk.edu.pl>
2016-12-10 11:39:39 +0100

    Sprawdzanie sumy kontrolnej

--- src/main/java/domain/helpers/PeselHelper.java
+++ src/main/java/domain/helpers/PeselHelper.java
@@ -8,6 +8,26 @@ public class PeselHelper {
         if (!pesel.matches("^[0-9]{11}$")) {
             return false;
         }
-        return true;
+        return checkSum(pesel);
+    }
+
+    private boolean checkSum(String pesel) {
+        int sum = (
+                1 * pesel[0]
+              + 3 * pesel[1]
+              + 7 * pesel[2]
+              + 9 * pesel[3]
+              + 1 * pesel[4]
+              + 3 * pesel[5]
+              + 7 * pesel[6]
+              + 9 * pesel[7]
+              + 1 * pesel[8]
+              + 3 * pesel[9]
+        );
+        sum %= 10;
+        sum = 10 - sum;
+        sum %= 10;
+
+        return (sum == pesel[10]);
     }
 }
