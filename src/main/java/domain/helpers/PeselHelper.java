c5905153e5440bd64141764fef53ddf9420fef2d
Marek Marecki <s14288@pjwstk.edu.pl>
2016-12-10 11:29:48 +0100

    Metody testowe mniejsze

--- src/main/java/domain/helpers/PeselHelper.java
+++ src/main/java/domain/helpers/PeselHelper.java
@@ -3,8 +3,6 @@ package domain.helpers;
 public class PeselHelper {
 
 	public static boolean checkPesel(String correctPesel) {
-		
-		
 		return true;
 	}
 
--- src/test/java/domain/helpers/PeselHelperTest.java
+++ src/test/java/domain/helpers/PeselHelperTest.java
@@ -5,20 +5,29 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class PeselHelperTest {
-
-	
+    public static String CORRECT_PESEL = "43061213866";
 	
 	@Test
-	public void CheckPeselTest() {
-		
-		String correctPesel = "43061213866";
-		boolean isCorrect = PeselHelper.checkPesel(correctPesel);
+	public void CheckCorrectPeselTest() {
+		boolean isCorrect = PeselHelper.checkPesel(CORRECT_PESEL);
 		assertTrue(isCorrect);
+    }
+
+	@Test
+	public void CheckPeselWithInvalidCharactersTest() {
 		String peselWithChar = "adcsadfsadcsa";
 		isCorrect=PeselHelper.checkPesel(peselWithChar);
 		assertFalse(isCorrect);
+    }
+
+	@Test
+	public void CheckPeselWithInvalidLengthTest() {
 		isCorrect = PeselHelper.checkPesel("213242353464576");
 		assertFalse(isCorrect);
+    }
+
+	@Test
+	public void CheckPeselWithInvalidChecksumTest() {
 		isCorrect = PeselHelper.checkPesel("43061213867");
 		assertFalse(isCorrect);
 	}
